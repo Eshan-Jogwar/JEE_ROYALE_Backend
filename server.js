012ce20b18ac32550d8188a18920defe751ee524
eshan jogwar <eshanjogwar2@gmail.com>
2025-04-02 20:12:41 +0530

    update questions feature done

--- server.js
+++ server.js
@@ -92,6 +92,87 @@ app.post("/getprofile", async (req, res) => {
     }
 })
 
+app.post("/getNumPeople", async (req, res) => {
+    const people = await client.liveSessions.findFirst({
+        select: {
+            people: true
+        },
+        where: {
+            sessionId: req.body.sessionId
+        }
+    })
+
+    res.json({people: people.people})
+})
+  
+app.get("/add-questions", async (req, res) => {
+    try {
+      const createdQuestions = await client.questions.createMany({
+        data: questions,
+        skipDuplicates: true, // Avoid inserting duplicates
+      });
+  
+      res.json({
+        message: "Questions added successfully!",
+        count: createdQuestions.count,
+      });
+    } catch (error) {
+      console.error("Error inserting questions:", error);
+      res.status(500).json({ error: "Failed to insert questions" });
+    }
+  });
+app.post("/updateQuestions", async (req, res) => {
+    try {
+        let questions = await client.$queryRaw`
+            SELECT "Question_Body", "Options", "correct_option" 
+            FROM "Questions" 
+            ORDER BY RANDOM() 
+            LIMIT 10
+        `;
+
+        let questions_list = [];
+        let options_list = [];
+        let correct_options_list = [];
+        for (let i = 0; i < questions.length; i++) {
+            questions_list.push(questions[i].Question_Body);
+            questions[i].Options.forEach(element => {
+                options_list.push(element);
+            });;
+            correct_options_list.push(questions[i].correct_option)
+        }
+
+        const addedQuestions = await client.liveSessionsQuestions.create({
+            data : {
+                sessionId: req.body.sessionId,
+                questions: questions_list,
+                options: options_list
+            }
+        })
+
+        const addedAnswers = await client.liveQuestionsAnswerTable.create({
+            data : {
+                sessionId: req.body.sessionId,
+                curr_answers: correct_options_list
+            }
+        })
+
+        res.json({ addedQuestions, addedAnswers });
+    } catch (error) {
+        console.error("Database query failed:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}) 
+
+
+app.post("/endSession", async (req, res) => {
+    const temp = await client.liveSessions.delete({
+        where: {
+            sessionId: req.body.sessionId 
+        }
+    })
+    if (temp) res.json(temp);
+})
+
 app.post("/startSession", async (req, res) => {
     const sessions = await client.liveSessions.findFirst({
         select: {
@@ -119,7 +200,7 @@ app.post("/startSession", async (req, res) => {
                     topic: req.body.topic
                 }
             });
-            res.json({sessionId: newSession.sessionId});
+            res.json({sessionId: newSession.sessionId, request: req.body});
         }else{
             await client.liveSessions.update({
                 where: {
@@ -132,7 +213,7 @@ app.post("/startSession", async (req, res) => {
                     people: true 
                 }
             });
-            res.json({sessionId: sessions.sessionId});
+            res.json({sessionId: sessions.sessionId, request: req.body});
         }
     }else{
         const newSession = await client.liveSessions.create({
@@ -142,7 +223,7 @@ app.post("/startSession", async (req, res) => {
                 topic: req.body.topic
             }
         });
-        res.json({sessionId: newSession.sessionId});
+        res.json({sessionId: newSession.sessionId, request: req.body});
     }
 })
 
